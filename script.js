f66e5eb70a797e33c1b19e598d3b4565cd493c63
Ehsan Karimi <ehsan.karimi.cryp@gmail.com>
2024-02-07 19:26:10 +0330

    timer is started with starting typing

--- script.js
+++ script.js
@@ -1,9 +1,21 @@
 const timerSection = document.querySelector(".numbers");
+const typeText = document.querySelector(".type-text");
 
 var timerNumbers = [0, 0, 0, 0];
 
+var y2 = 5;
+
+function y(y1) {
+  if (y1 <= 9) {
+    y1 = "0" + y1;
+  }
+
+  return y1;
+}
+
 function timerFunction() {
-  var z = timerNumbers[0] + ":" + timerNumbers[1] + ":" + timerNumbers[2];
+  var z =
+    y(timerNumbers[0]) + ":" + y(timerNumbers[1]) + ":" + y(timerNumbers[2]);
   timerSection.innerHTML = z;
 
   timerNumbers[3]++;
@@ -14,5 +26,14 @@ function timerFunction() {
     timerNumbers[3] - timerNumbers[1] * 100 - timerNumbers[0] * 6000
   );
 }
+function timerStart() {
 
-setInterval(timerFunction , 10);
+  if(y2 == 5) {
+    setInterval(timerFunction, 10);
+    y2 = y2 + 1;
+
+  }
+  
+}
+
+typeText.addEventListener("keypress", timerStart);
