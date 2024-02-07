96a7a380c092ab7794a73bd8604caf53b27ee086
Ehsan Karimi <ehsan.karimi.cryp@gmail.com>
2024-02-07 22:00:16 +0330

    You can use project

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
 const timerSection = document.querySelector(".numbers");
 const typeText = document.querySelector(".type-text");
+const textShouldBeTyped = document.querySelector(
+  ".text-that-should-be-typed"
+).innerHTML;
+const textButton = document.querySelector(".restart-button");
 
 var timerNumbers = [0, 0, 0, 0];
 
 var y2 = 5;
+var y3;
 
 function y(y1) {
   if (y1 <= 9) {
@@ -26,14 +31,41 @@ function timerFunction() {
     timerNumbers[3] - timerNumbers[1] * 100 - timerNumbers[0] * 6000
   );
 }
-function timerStart() {
 
-  if(y2 == 5) {
-    setInterval(timerFunction, 10);
-    y2 = y2 + 1;
-
+function textTyped() {
+  let userText = typeText.value;
+  let textShoudBeTypedAndUserText = textShouldBeTyped.substring(
+    0,
+    userText.length
+  );
+  if (userText == textShouldBeTyped) {
+    typeText.style.backgroundColor = "#96e4bd";
+    clearInterval(y3);
+  } else {
+    if (userText == textShoudBeTypedAndUserText) {
+      typeText.style.backgroundColor = "#bd7db4";
+    } else {
+      typeText.style.backgroundColor = "#ff4747";
+    }
   }
-  
+}
+function buttonFunction() {
+  clearInterval(y3);
+  y3 = null;
+  timerNumbers = [0, 0, 0, 0];
+  y2 = 5;
+  typeText.value = "";
+  timerSection.innerHTML = "00:00:00";
+  typeText.style.backgroundColor = "#c6e8e9";
+}
+
+function timerStart() {
+  if (y2 == 5) {
+    y3 = setInterval(timerFunction, 10);
+    y2 = y2 + 1;
+  }
 }
 
 typeText.addEventListener("keypress", timerStart);
+typeText.addEventListener("keyup", textTyped);
+textButton.addEventListener("click", buttonFunction);
